de07526547b78aea8ba67cae87bcad547303b068
wellechi <wellechi03@gmail.com>
2024-11-04 13:36:56 -0300

    1.0

---
+++ js/main.js
@@ -0,0 +1,91 @@
+function alternarGabarito1() {
+    var gabarito = document.getElementById("gabarito1");
+    var botao = document.getElementById("botao1");
+
+    if (gabarito.style.display === "none") {
+        gabarito.style.display = "block";
+        botao.textContent = "Esconder Resposta";
+        botao.style.backgroundColor = "red"; 
+    } else {
+        gabarito.style.display = "none";
+        botao.textContent = "Resposta";
+        botao.style.backgroundColor = "#1E2A78"; 
+    }
+}
+
+function alternarGabarito2() {
+    var gabarito = document.getElementById("gabarito2");
+    var botao = document.getElementById("botao2");
+
+    if (gabarito.style.display === "none") {
+        gabarito.style.display = "block";
+        botao.textContent = "Esconder Resposta";
+        botao.style.backgroundColor = "red"; 
+    } else {
+        gabarito.style.display = "none";
+        botao.textContent = "Resposta";
+        botao.style.backgroundColor = "#1E2A78"; 
+    }
+}
+
+function alternarGabarito3() {
+    var gabarito = document.getElementById("gabarito3");
+    var botao = document.getElementById("botao3");
+
+    if (gabarito.style.display === "none") {
+        gabarito.style.display = "block";
+        botao.textContent = "Esconder Resposta";
+        botao.style.backgroundColor = "red"; 
+    } else {
+        gabarito.style.display = "none";
+        botao.textContent = "Resposta";
+        botao.style.backgroundColor = "#1E2A78"; 
+    }
+}
+
+function alternarGabarito4() {
+    var gabarito = document.getElementById("gabarito4");
+    var botao = document.getElementById("botao4");
+
+    if (gabarito.style.display === "none") {
+        gabarito.style.display = "block";
+        botao.textContent = "Esconder Resposta";
+        botao.style.backgroundColor = "red"; 
+    } else {
+        gabarito.style.display = "none";
+        botao.textContent = "Resposta";
+        botao.style.backgroundColor = "#1E2A78"; 
+    }
+}
+
+function alternarGabarito5() {
+    var gabarito = document.getElementById("gabarito5");
+    var botao = document.getElementById("botao5");
+
+    if (gabarito.style.display === "none") {
+        gabarito.style.display = "block";
+        botao.textContent = "Esconder Resposta";
+        botao.style.backgroundColor = "red"; 
+    } else {
+        gabarito.style.display = "none";
+        botao.textContent = "Resposta";
+        botao.style.backgroundColor = "#1E2A78"; 
+    }
+}
+
+function alternarGabarito6() {
+    var gabarito = document.getElementById("gabarito6");
+    var botao = document.getElementById("botao6");
+
+    if (gabarito.style.display === "none") {
+        gabarito.style.display = "block";
+        botao.textContent = "Esconder Resposta";
+        botao.style.backgroundColor = "red"; 
+    } else {
+        gabarito.style.display = "none";
+        botao.textContent = "Resposta";
+        botao.style.backgroundColor = "#1E2A78"; 
+    }
+}
+
+
